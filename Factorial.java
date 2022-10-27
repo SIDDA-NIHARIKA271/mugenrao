96f7c1a543911236910e385879db1338bcfbe4ae
knowledge <niharikasidda271@gmail.com>
2022-10-27 11:42:32 +0530

    first commit

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package first;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,fact=1;
+		int number=9;
+		for (i=1;i<=number;i++) {
+			fact=fact*i;
+		}
+		System.out.println("factorial :" +fact);
+		}
+
+	}
+
+ 
